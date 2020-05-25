7add7c0624aa04c18ff82b8a548df002e40b4297
Michael Burge <michaelburge@pobox.com>
2020-05-24 23:17:13 -0700

    Test identify_string

--- tests/test.rs
+++ tests/test.rs
@@ -3,9 +3,15 @@ use tt_identifier::{identify, identify_string};
 
 #[test]
 fn it_works() {
+    // Test that the identifier can be used in declarations & expressions.
     let identify!(Foo<Bar<(u64, u64)>>) = 2;
     assert_eq!(
         identify!(Foo<Bar<(u64, u64)>>) + identify!(Foo<Bar<(u64, u64)>>),
         4
+    );
+    // Test that the string equivalent doesn't throw an error.
+    assert_eq!(
+        identify_string!(Foo<Bar<(u64, u64)>>),
+        identify_string!(Foo<Bar<(u64, u64)>>)
     );
 }
